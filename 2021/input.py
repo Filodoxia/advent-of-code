bd603eb90fee249991721e5efbb81ac7398e9145
Filodoxia <Filodoxia@users.noreply.github.com>
2021-12-22 04:07:13 +0100

    Added additional method to read input files

--- 2021/input.py
+++ 2021/input.py
@@ -3,3 +3,22 @@ from os import path
 
 def read(day: int):
     return open(path.join(path.dirname(__file__), "input", f"day{day}"), "r")
+
+
+def readIntArray(day: int):
+    filePath = path.join(path.dirname(__file__), "input", f"day{day}")
+    with open(filePath, "r") as input:
+        a = list(map(int, input.readline().split(",")))
+    return a
+
+
+def readIntArray2d(day: int, separator: str = ","):
+    filePath = path.join(path.dirname(__file__), "input", f"day{day}")
+    a = []
+    with open(filePath, "r") as input:
+        for line in input:
+            if separator:
+                a.append(list(map(int, line.split(","))))
+            else:
+                a.append(list(map(int, list(line.strip()))))
+    return a
