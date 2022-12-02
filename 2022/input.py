b0737d1a07d40d2b495dbac91ac73c250ba2a9dc
Filodoxia <Filodoxia@users.noreply.github.com>
2022-12-02 01:12:15 +0100

    Added dedicated module to handle puzle input

---
+++ 2022/input.py
@@ -0,0 +1,26 @@
+from os import path
+
+
+def _getFilePath(filename: str):
+    file_path = path.join(path.abspath(path.curdir), "/input/", filename)
+    if not path.isfile(file_path):
+        raise Exception("Input file not found")
+    return file_path
+
+
+def read(filename: str):
+    file_content = ""
+
+    with open(_getFilePath(filename), "r") as f:
+        file_content = f.read()
+
+    return file_content
+
+
+def readLines(filename: str):
+    file_content: list[str] = []
+
+    with open(_getFilePath(filename), "r") as f:
+        file_content = f.readlines()
+
+    return file_content
